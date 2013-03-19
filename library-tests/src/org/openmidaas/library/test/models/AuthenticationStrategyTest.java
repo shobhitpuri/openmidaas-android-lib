66bdaeaa684704150b2e455197ec7ed36b7ae135
adas2k <adas2k@gmail.com>
2013-03-19 09:59:53 -0400

    some auth tests

--- library-tests/src/org/openmidaas/library/test/models/AuthenticationStrategyTest.java
+++ library-tests/src/org/openmidaas/library/test/models/AuthenticationStrategyTest.java
@@ -17,43 +17,36 @@ package org.openmidaas.library.test.models;
 
 import java.util.concurrent.CountDownLatch;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.openmidaas.library.MIDaaS;
-import org.openmidaas.library.common.network.ConnectionManager;
+import org.openmidaas.library.model.core.AuthenticationCallback;
 import org.openmidaas.library.model.core.DeviceIdAuthentication;
-import org.openmidaas.library.model.core.DeviceRegistration;
-import org.openmidaas.library.model.core.InitializationCallback;
 import org.openmidaas.library.model.core.MIDaaSException;
-import org.openmidaas.library.test.network.MockTransportFactory;
 
 import android.content.Context;
 import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
 
-public class DeviceRegistrationTest extends InstrumentationTestCase{
+public class AuthenticationStrategyTest extends InstrumentationTestCase {
 	
-	private MockTransportFactory mockFactory;
 	private Context mContext;
 	private boolean notificationSuccess = false;
-	
+	private String deviceToken;
 	protected void setUp() throws Exception {
 		mContext = getInstrumentation().getContext();
-		mockFactory = new MockTransportFactory(mContext, "device_reg_success.json");
 		MIDaaS.setContext(mContext);
-		ConnectionManager.setNetworkFactory(mockFactory);
 	}
 	
 	@SmallTest
-	public void testRegistrationSuccess() throws Exception {
+	public void testDeviceIDAuthenticationStrategy() throws Exception {
 		final CountDownLatch mLatch = new CountDownLatch(1);
-		
-		DeviceRegistration deviceRegistration = new DeviceRegistration(new DeviceIdAuthentication());
-		deviceRegistration.registerDevice(new InitializationCallback() {
+		DeviceIdAuthentication deviceAuth = new DeviceIdAuthentication();
+		deviceAuth.performAuthentication(new AuthenticationCallback() {
 
 			@Override
-			public void onSuccess() {
+			public void onSuccess(String deviceId) {
 				notificationSuccess = true;
+				deviceToken = deviceId;
 				mLatch.countDown();
 			}
 
@@ -61,63 +54,34 @@ public class DeviceRegistrationTest extends InstrumentationTestCase{
 			public void onError(MIDaaSException exception) {
 				notificationSuccess = false;
 				mLatch.countDown();
-			}	
+			}
 		});
-		
 		mLatch.await();
 		Assert.assertTrue(notificationSuccess);
+		Assert.assertNotNull(deviceToken);
 	}
 	
 	@SmallTest
-	public void testRegistrationFailure() throws Exception {
+	public void testErrorInAuthenticationCallback() throws Exception {
 		final CountDownLatch mLatch = new CountDownLatch(1);
-		mockFactory.setFilename("device_reg_fail.json");
-		DeviceRegistration deviceRegistration = new DeviceRegistration(new DeviceIdAuthentication());
-		deviceRegistration.registerDevice(new InitializationCallback() {
+		MockAuthenticationStrategy deviceAuth = new MockAuthenticationStrategy();
+		deviceAuth.performAuthentication(new AuthenticationCallback() {
 
-			
+			@Override
+			public void onSuccess(String deviceId) {
+				notificationSuccess = true;
+				deviceToken = deviceId;
+				mLatch.countDown();
+			}
 
 			@Override
 			public void onError(MIDaaSException exception) {
 				notificationSuccess = false;
 				mLatch.countDown();
 			}
-
-			@Override
-			public void onSuccess() {
-				notificationSuccess = true;
-				mLatch.countDown();
-			}	
 		});
-		
 		mLatch.await();
 		Assert.assertEquals(false, notificationSuccess);
 	}
 	
-	@SmallTest
-	public void testRegistrationFailureWithErrorInAuthenticationStrategy()  throws Exception {
-		final CountDownLatch mLatch = new CountDownLatch(1);
-		mockFactory.setFilename("device_reg_success.json");
-		DeviceRegistration deviceRegistration = new DeviceRegistration(new MockAuthenticationStrategy());
-		deviceRegistration.registerDevice(new InitializationCallback() {
-
-			
-
-			@Override
-			public void onError(MIDaaSException exception) {
-				notificationSuccess = false;
-				mLatch.countDown();
-			}
-
-			@Override
-			public void onSuccess() {
-				notificationSuccess = true;
-				mLatch.countDown();
-			}	
-		});
-		
-		mLatch.await();
-		Assert.assertEquals(false, notificationSuccess);
-	}
-
 }
